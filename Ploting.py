da5a7adc62020daff08ce29724acc0850446191d
giorgoskoufetidis <giorgos7toni@gmail.com>
2024-12-23 17:45:28 +0200

    Ploting the M-R diagrams for txt file.

--- Ploting.py
+++ Ploting.py
@@ -1,20 +1,8 @@
-
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 24 17:13:20 2024
-
-@author: User
-"""
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 file_path = "TOV_results_for_multiple_models (1).txt"
-
 results = {}
-
 with open(file_path, "r") as f:
     lines = f.readlines()
 
@@ -35,16 +23,11 @@ if current_model and data:
     df = pd.DataFrame(data,columns = ["Mass","Radius","Pressure"])
     results[current_model] = df
 
-
-#for model_name, df in results.items():
-#    print(f"Model:{model_name}")
-
 model_names = list(results.keys())
 split_index = len(model_names) // 2
 group_1 = model_names[:split_index]
 group_2 = model_names[split_index:]
 
-# Plot 1: First half of the models
 plt.figure(figsize=(10, 6))
 for model_name in group_1:
     df = results[model_name]
@@ -69,18 +52,3 @@ plt.legend()
 plt.grid(True)
 plt.show()
 plt.savefig("Figure_2.jpg")
-
-
-'''
-# Example: Plot Pressure vs Radius for all models
-for model_name, df in results.items():
-    plt.figure(figsize=(8, 6))
-    plt.plot(df["Radius"], df["Pressure"], label=f"{model_name}: Pressure vs Radius", color="green")
-    plt.xlabel("Radius (km)")
-    plt.ylabel("Pressure")
-    plt.title(f"Pressure-Radius Relation ({model_name})")
-    plt.legend()
-    plt.grid(True)
-  '''
-
-
